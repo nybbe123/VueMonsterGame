25cafa8b63d3e93a188f9b58e50819e872a845c7
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2022-06-22 11:29:00 +0200

    battle log logic

--- app.js
+++ app.js
@@ -9,17 +9,19 @@ const app = Vue.createApp({
       monsterHealth: 100,
       specialAttackValue: 0,
       battleLogs: [],
+      logMessage: "",
+      gameOverStatus: "",
     };
   },
   watch: {
     playerHealth(value) {
       if (value <= 0) {
-        alert("monster has defeated you...");
+        this.gameOverStatus = "Monster defeated you...";
       }
     },
     monsterHealth(value) {
       if (value <= 0) {
-        alert("You defeated the monster!");
+        this.gameOverStatus = "You defeated monster!";
       }
     },
   },
@@ -30,30 +32,58 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: `${this.playerHealth}%` };
     },
+    specialAttackClass() {
+      return { inactive: this.specialAttackValue < 5 };
+    },
   },
   methods: {
     attackMonster() {
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.specialAttackValue += 1;
+      this.addLogMessage("player", "attack", attackValue);
       this.attackPlayer();
     },
     specialAttackMonster() {
       if (this.specialAttackValue >= 5) {
-        const attackValue = getRandomValue(20, 35);
+        const attackValue = getRandomValue(10, 25);
         this.monsterHealth -= attackValue;
         this.specialAttackValue = 0;
+        this.addLogMessage("monster", "attack", attackValue);
         this.attackPlayer();
       }
     },
     healPlayer() {
-      const healValue = getRandomValue(12, 18);
-      this.playerHealth += healValue;
+      const healValue = getRandomValue(8, 24);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.addLogMessage("player", "heal", healValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage("monster", "attack", attackValue);
+    },
+    surrender() {
+      this.playerHealth = 0;
+    },
+    resetGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.specialAttackValue = 0;
+      this.battleLogs = [];
+      this.gameOverStatus = "";
+    },
+    addLogMessage(who, what, value) {
+      this.battleLogs.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 });
